6e2111b2a074bb573e574375d3f3e10f7aea0db3
Varun Joshi <joshvarun@gmail.com>
2019-02-26 22:42:57 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponentComponent } from './components/user-component/user-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponentComponent
   ],
   imports: [
     BrowserModule,
